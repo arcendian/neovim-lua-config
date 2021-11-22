e17056f48ca32326030b6a8844977bbe8e9efd4e
Michael Espeña <lee.de.roy.124@gmail.com>
2021-11-22 09:57:17 +0800

    changed separators for lualine

--- lua/statusline.lua
+++ lua/statusline.lua
@@ -1,5 +1,7 @@
 require('lualine').setup {
   options = {
-    theme = 'onenord'
+    theme = 'onenord',
+    component_separators = '|',
+    section_separators = { left = '', right = '' },
   }
 }
