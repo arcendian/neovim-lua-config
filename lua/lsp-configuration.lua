57e96d4074a4595977a60173ca8a85ee6d35a8b3
Michael Espeña <lee.de.roy.124@gmail.com>
2021-11-24 16:04:37 +0800

    hint prefix config for lsp signature

--- lua/lsp-configuration.lua
+++ lua/lsp-configuration.lua
@@ -22,3 +22,5 @@ require('lspconfig').bashls.setup {
 -- require('lspconfig').hls.setup {
 --   capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- }
+
+require('lsp_signature').setup {  hint_prefix = '💡 ' } 
