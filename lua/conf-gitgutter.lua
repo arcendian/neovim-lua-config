a76cf2d2b6a26e9a3184a4f12ee81ab8404fed30
Michael Espeña <lee.de.roy.124@gmail.com>
2021-11-24 21:57:20 +0800

    changed sign for modified removed

--- lua/conf-gitgutter.lua
+++ lua/conf-gitgutter.lua
@@ -3,7 +3,7 @@ vim.g.gitgutter_sign_modified = ' '
 vim.g.gitgutter_sign_removed = ' '
 vim.g.gitgutter_sign_removed_first_line = ''
 vim.g.gitgutter_sign_removed_above_and_below = ' '
-vim.g.gitgutter_sign_modified_removed = ''
+vim.g.gitgutter_sign_modified_removed = ''
 
 vim.g.gitgutter_enabled = 1
 -- vim.api.nvim_set_keymap('n', '<leader>te', ':GitGutterEnable<CR>', { noremap = true, silent = true}) 
