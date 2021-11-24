34a43c7d05e87ac146423bb3283cf2eeddc714ea
Michael Espeña <lee.de.roy.124@gmail.com>
2021-11-24 20:55:10 +0800

    minor edit

--- init.lua
+++ init.lua
@@ -15,13 +15,13 @@ require('remaps')
 require('floaterm')
 require'nvim-web-devicons'.setup { default = true; }
 require('file-explorer')
-require("todo-comments").setup { }
+require('todo-comments').setup { }
 require('conf-tree-sitter')
 require('autopairs')
 require('completion')
 require('conf-lspconfig')
 require('conf-telescope')
 require('md-preview')
-require("which-key").setup { }
+require('which-key').setup { }
 require('writing')
 require('dashboard')
