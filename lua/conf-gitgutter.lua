4ebc8a41821b4d8a91f274782135f5648fcf6890
Michael Espeña <lee.de.roy.124@gmail.com>
2021-11-25 15:43:51 +0800

    changed gitgutter keybindings

--- lua/conf-gitgutter.lua
+++ lua/conf-gitgutter.lua
@@ -1,3 +1,4 @@
+vim.g.gitgutter_map_keys = 0
 vim.g.gitgutter_sign_added = ' '
 vim.g.gitgutter_sign_modified = ' '
 vim.g.gitgutter_sign_removed = ' '
@@ -6,6 +7,9 @@ vim.g.gitgutter_sign_removed_above_and_below = ' '
 vim.g.gitgutter_sign_modified_removed = ''
 
 vim.g.gitgutter_enabled = 1
--- vim.api.nvim_set_keymap('n', '<leader>te', ':GitGutterEnable<CR>', { noremap = true, silent = true}) 
--- vim.api.nvim_set_keymap('n', '<leader>td', ':GitGutterDisable<CR>', { noremap = true, silent = true}) 
--- vim.api.nvim_set_keymap('n', '<leader>tt', ':GitGutterToggle<CR>', { noremap = true, silent = true}) 
+vim.api.nvim_set_keymap('n', '<leader>he', ':GitGutterEnable<CR>', { noremap = true, silent = true}) 
+vim.api.nvim_set_keymap('n', '<leader>hd', ':GitGutterDisable<CR>', { noremap = true, silent = true}) 
+vim.api.nvim_set_keymap('n', '<leader>ht', ':GitGutterToggle<CR>', { noremap = true, silent = true}) 
+vim.api.nvim_set_keymap('n', '<leader>hp', ':GitGutterPreviewHunk<CR>', { noremap = true, silent = true}) 
+vim.api.nvim_set_keymap('n', '<leader>hs', ':GitGutterStageHunk<CR>', { noremap = true, silent = true}) 
+vim.api.nvim_set_keymap('n', '<leader>hu', ':GitGutterUndoHunk<CR>', { noremap = true, silent = true}) 
