eed9ce2f45cfa5df62f97c087efc24366c8e96c6
Michael Espeña <lee.de.roy.124@gmail.com>
2021-11-27 15:44:05 +0800

    added: vim-language-server to lspconfig

--- lua/conf-lspconfig.lua
+++ lua/conf-lspconfig.lua
@@ -40,6 +40,11 @@ lspconfig.texlab.setup {
                                                                    .make_client_capabilities())
 }
 
+lspconfig.vimls.setup {
+    capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol
+                                                                   .make_client_capabilities())
+}
+
 lspconfig.arduino_language_server.setup({
     cmd = {
         "arduino-language-server", "-cli-config",
