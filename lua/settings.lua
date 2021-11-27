0046dcbfccdf111310b8a161cbb5c761c66244e2
Michael Espeña <lee.de.roy.124@gmail.com>
2021-11-27 11:33:16 +0800

    added: haskell settings

--- lua/settings.lua
+++ lua/settings.lua
@@ -2,7 +2,7 @@
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.expandtab = false
 vim.opt.updatetime = 100
 vim.opt.number = true
 vim.opt.relativenumber = true
