12878457a1921f54df5ac809a3faf9cb35ef8e7d
Michael Espeña <lee.de.roy.124@gmail.com>
2021-11-27 17:21:20 +0800

    translated wildignore setting to lua

--- lua/settings.lua
+++ lua/settings.lua
@@ -25,9 +25,9 @@ vim.opt.showmode = false
 vim.opt.signcolumn = "yes"
 vim.opt.timeoutlen = 500
 
-vim.cmd([[
-    set wildignore=*.git/*,*.tags,tags,*.o,*.class,*.ccls-cache
-]])
+vim.g.wildignore = {
+    '*.git/*', '*.tags', 'tags', '*.o', '*.class', '*.ccls-cache'
+}
 
 -- highlight yanked text
 vim.highlight.on_yank {higroup = "IncSearch", timeout = 150}
