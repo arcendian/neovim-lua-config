0d1d49438d01988325d4b2b0c2ebf1b4e20d90a3
Michael Espeña <lee.de.roy.124@gmail.com>
2021-11-28 20:12:18 +0800

    removed lsp kind plugin

--- lua/completion/init.lua
+++ lua/completion/init.lua
@@ -1,5 +1,32 @@
 local cmp = require 'cmp'
-local lspkind = require('lspkind')
+
+local kind_icons = {
+    Text = "",
+    Method = "",
+    Function = "",
+    Constructor = "",
+    Field = "",
+    Variable = "",
+    Class = "ﴯ",
+    Interface = "",
+    Module = "",
+    Property = "ﰠ",
+    Unit = "",
+    Value = "",
+    Enum = "",
+    Keyword = "",
+    Snippet = "",
+    Color = "",
+    File = "",
+    Reference = "",
+    Folder = "",
+    EnumMember = "",
+    Constant = "",
+    Struct = "",
+    Event = "",
+    Operator = "",
+    TypeParameter = ""
+}
 
 cmp.setup({
     snippet = {expand = function(args) vim.fn["vsnip#anonymous"](args.body) end},
@@ -15,16 +42,23 @@ cmp.setup({
         {name = 'buffer'}, {name = 'path'}, {name = 'cmdline'}
     },
     formatting = {
-        format = lspkind.cmp_format({
-            with_text = true,
-            maxwidth = 50,
-            menu = ({
+        format = function(entry, vim_item)
+            -- Kind icons
+            vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind],
+                                          vim_item.kind) -- This concatonates the icons with the name of the item kind
+            -- Source
+            vim_item.menu = ({
                 buffer = "[Buffer]",
                 nvim_lsp = "[LSP]",
-                vsnip = "[Vsnip]",
+                luasnip = "[LuaSnip]",
                 nvim_lua = "[Lua]",
-                latex_symbols = "[Latex]"
-            })
-        })
+                latex_symbols = "[LaTeX]"
+            })[entry.source.name]
+            return vim_item
+        end
     }
 })
+
+-- lsp signature
+require"lsp_signature".setup({cfg = {hint_prefix = "💡 "}})
+
