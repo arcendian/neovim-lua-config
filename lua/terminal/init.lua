471ddd43926c2ee178fb302b8febd63214f67b4d
Michael Espeña <lee.de.roy.124@gmail.com>
2021-11-29 07:02:13 +0800

    minor terminal tweaks

--- lua/terminal/init.lua
+++ lua/terminal/init.lua
@@ -1,15 +1,12 @@
 require('toggleterm').setup {
     open_mapping = [[<c-\>]],
     direction = 'float',
+    shade_terminals = false,
     float_opts = {
-        border = 'single', -- | 'double' | 'shadow' | 'curved' | ... other options supported by win open
+        border = 'single',
         -- width = <value>,
         -- height = <value>,
         winblend = 3,
         highlights = {border = "Normal", background = "Normal"}
     }
 }
--- The border key is *almost* the same as 'nvim_open_win'
--- see :h nvim_open_win for details on borders however
--- the 'curved' border is a custom border type
--- not natively supported but implemented in this plugin.
