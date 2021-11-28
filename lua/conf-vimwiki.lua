5636bd9645c8e552e85357d1e316484df00e3284
Michael Espeña <lee.de.roy.124@gmail.com>
2021-11-28 09:07:45 +0800

    minor edit

--- lua/conf-vimwiki.lua
+++ lua/conf-vimwiki.lua
@@ -15,7 +15,7 @@ vim.cmd([[
 vim.g.vimwiki_global_ext = 0
 
 -- TODO: How to translate the above to Lua. Too lazy to search for now.
--- vim.g.vimwiki_list = ({
+-- vim.g.vimwiki_list = {
 --     path = '~/Documents/VimWiki/Default/',
 --     path_tasks = '~/Documents/VimWiki/Tasks/',
 --     path_calendar = '~/Documents/VimWiki/Calendar/',
@@ -25,6 +25,4 @@ vim.g.vimwiki_global_ext = 0
 --     path_random = '~/Documents/VimWiki/Random/',
 --     path_review = '~/Documents/VimWiki/Review/',
 --     path_worldbuilding = '~/Documents/VimWiki/Worldbuilding/',
---     syntax = 'markdown',
---     ext = '.md'
--- })
+-- }
