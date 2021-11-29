22918f5247b763eee581566ad086ff6a39a2238a
Michael Espeña <lee.de.roy.124@gmail.com>
2021-11-29 20:07:06 +0800

    disable indent-blankline in help buffer

--- lua/plugins/cfg-indent-blankline/init.lua
+++ lua/plugins/cfg-indent-blankline/init.lua
@@ -1,3 +1,3 @@
 vim.opt.list = true
-vim.g.indentLine_fileTypeExclude = {'dashboard', 'packer'}
+vim.g.indentLine_fileTypeExclude = {'dashboard', 'packer', 'help'}
 require('indent_blankline').setup {}
