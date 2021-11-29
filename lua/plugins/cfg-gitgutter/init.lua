85ab754f2387373fd13da18746b5998080cede03
Michael Espeña <lee.de.roy.124@gmail.com>
2021-11-29 19:24:56 +0800

    commented out custom signs; too distracting

--- lua/plugins/cfg-gitgutter/init.lua
+++ lua/plugins/cfg-gitgutter/init.lua
@@ -1,10 +1,10 @@
 vim.g.gitgutter_map_keys = 0
-vim.g.gitgutter_sign_added = ' '
-vim.g.gitgutter_sign_modified = ' '
-vim.g.gitgutter_sign_removed = ' '
-vim.g.gitgutter_sign_removed_first_line = ''
-vim.g.gitgutter_sign_removed_above_and_below = ' '
-vim.g.gitgutter_sign_modified_removed = ''
+-- vim.g.gitgutter_sign_added = ' '
+-- vim.g.gitgutter_sign_modified = ' '
+-- vim.g.gitgutter_sign_removed = ' '
+-- vim.g.gitgutter_sign_removed_first_line = ''
+-- vim.g.gitgutter_sign_removed_above_and_below = ' '
+-- vim.g.gitgutter_sign_modified_removed = ''
 
 vim.g.gitgutter_enabled = 1
 vim.api.nvim_set_keymap('n', '<leader>he', ':GitGutterEnable<CR>',
