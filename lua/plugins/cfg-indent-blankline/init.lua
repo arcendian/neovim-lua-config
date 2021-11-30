283cad7035d40168b12e631e2b8a18442ae164ee
Michael Espeña <lee.de.roy.124@gmail.com>
2021-11-30 15:34:04 +0800

    disable indent-blankline in toggle-term and terminal(?)

--- lua/plugins/cfg-indent-blankline/init.lua
+++ lua/plugins/cfg-indent-blankline/init.lua
@@ -1,3 +1,10 @@
 vim.opt.list = true
-vim.g.indentLine_fileTypeExclude = {'dashboard', 'packer', 'help'}
+-- NOTE: it doesn't show in the statusline what the buffer is called when
+-- using builtin terminal so I just put an empty string which seems to do
+-- the trick of disabling indent_blankline in terminal;
+-- TODO: documentations show what the terminal buffer is called but I don't
+-- know how to put the name properly here for the moment
+vim.g.indentLine_fileTypeExclude = {
+    'dashboard', 'packer', 'help', 'ToggleTerm', ''
+}
 require('indent_blankline').setup {}
