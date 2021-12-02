abcec45eba9d01ce8810d1f4129a3f0726b2de02
Michael Espeña <lee.de.roy.124@gmail.com>
2021-12-02 11:40:18 +0800

    minor format edit; changed to lua version orgmode

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -16,22 +16,22 @@ end
 return require("packer").startup(function()
 	-- ======================= UTILITIES ================================== --
 	-- Packer can manage itself
-	use("wbthomason/packer.nvim")
+	use({ "wbthomason/packer.nvim" })
 
 	-- lsp configurations
-	use("neovim/nvim-lspconfig")
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-cmdline")
-	use("hrsh7th/nvim-cmp")
-	use("ray-x/lsp_signature.nvim")
+	use({ "neovim/nvim-lspconfig" })
+	use({ "hrsh7th/cmp-nvim-lsp" })
+	use({ "hrsh7th/cmp-buffer" })
+	use({ "hrsh7th/cmp-path" })
+	use({ "hrsh7th/cmp-cmdline" })
+	use({ "hrsh7th/nvim-cmp" })
+	use({ "ray-x/lsp_signature.nvim" })
 
 	-- snippets
-	use("hrsh7th/vim-vsnip")
-	use("hrsh7th/vim-vsnip-integ")
-	use("hrsh7th/cmp-vsnip")
-	use("rafamadriz/friendly-snippets")
+	use({ "hrsh7th/vim-vsnip" })
+	use({ "hrsh7th/vim-vsnip-integ" })
+	use({ "hrsh7th/cmp-vsnip" })
+	use({ "rafamadriz/friendly-snippets" })
 
 	-- telescope - fuzzy finder and more
 	use({
@@ -41,36 +41,36 @@ return require("packer").startup(function()
 	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
 
 	-- auto-pairing brackets
-	use("windwp/nvim-autopairs")
+	use({ "windwp/nvim-autopairs" })
 
 	-- keybindings
-	use("folke/which-key.nvim")
+	use({ "folke/which-key.nvim" })
 
 	-- essential web dev tool I just want to try out
-	use("mattn/emmet-vim")
+	use({ "mattn/emmet-vim" })
 
 	-- autoformatting -- trying out this plugin for now
-	use("mhartington/formatter.nvim")
+	use({ "mhartington/formatter.nvim" })
 
 	-- plugin for easier use of builtin terminal
-	use("akinsho/toggleterm.nvim")
+	use({ "akinsho/toggleterm.nvim" })
 
 	-- markdown preview in browser
 	use({ "iamcco/markdown-preview.nvim", run = "cd app && yarn install" })
 
 	-- git stuff
-	use("tpope/vim-fugitive")
-	use("airblade/vim-gitgutter")
+	use({ "tpope/vim-fugitive" })
+	use({ "airblade/vim-gitgutter" })
 
 	-- ARM assembly syntax hightlighting
-	use("ARM9/arm-syntax-vim")
+	use({ "ARM9/arm-syntax-vim" })
 
 	-- ========================== UI ====================================== --
 	-- better syntax highlighting with tree-sitter and more
 	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
 
 	-- colorscheme
-	use("rmehri01/onenord.nvim")
+	use({ "rmehri01/onenord.nvim" })
 
 	-- fancy statusline
 	use({
@@ -79,30 +79,30 @@ return require("packer").startup(function()
 	})
 
 	-- comments plugins
-	use("terrortylor/nvim-comment") -- maybe I don't need this but ...
+	use({ "terrortylor/nvim-comment" }) -- maybe I don't need this but ...
 
 	-- dashboard
-	use("glepnir/dashboard-nvim")
+	use({ "glepnir/dashboard-nvim" })
 
 	-- add indentation guides to all lines (including empty lines).
-	use("lukas-reineke/indent-blankline.nvim")
+	use({ "lukas-reineke/indent-blankline.nvim" })
 
 	-- colors for hex color values
 	use({ "rrethy/vim-hexokinase", run = "make hexokinase" })
 
 	-- icons/glyphs
-	use("kyazdani42/nvim-web-devicons")
+	use({ "kyazdani42/nvim-web-devicons" })
 
 	-- file-explorer (netrw is nice but this looks nicer)
 	use({ "kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons" })
 
 	-- ======================= WRITING ================================== --
 	--  plugins for writing
-	use("folke/zen-mode.nvim")
-	use("folke/twilight.nvim")
-	use("vimwiki/vimwiki")
+	use({ "folke/zen-mode.nvim" })
+	use({ "folke/twilight.nvim" })
+	use({ "vimwiki/vimwiki" })
 
 	-- installing org.vim so I can view org files in vim with syntax
 	-- highlighting in case Doom Emacs breaks on me.
-	use("axvr/org.vim")
+	use({ "nvim-orgmode/orgmode" })
 end)
