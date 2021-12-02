e6f7ea9d4cbc80a172e2341572bddb82c5c0c9ce
Michael Espeña <lee.de.roy.124@gmail.com>
2021-12-02 09:13:11 +0800

    changed some autopairs settings

--- lua/plugins/cfg-autopairs/init.lua
+++ lua/plugins/cfg-autopairs/init.lua
@@ -5,17 +5,15 @@ npairs.setup({
 	ts_config = {
 		lua = { "string" }, -- it will not add a pair on that treesitter node
 		javascript = { "template_string" },
-		java = false, -- don't check treesitter on java
 	},
 	map_cr = true,
+	fast_wrap = {},
 })
 
--- local ts_conds = require("nvim-autopairs.ts-conds")
+-- Insert `(` after select function or method item
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local cmp = require("cmp")
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
 
----- press % => %% is only inside comment or string
--- npairs.add_rules({
---  Rule("%", "%", "lua")
---    :with_pair(ts_conds.is_ts_node({'string','comment'})),
---  Rule("$", "$", "lua")
---    :with_pair(ts_conds.is_not_ts_node({'function'}))
--- })
+-- add a lisp filetype (wrap my-function), FYI: Hardcoded = { "clojure", "clojurescript", "fennel", "janet" }
+cmp_autopairs.lisp[#cmp_autopairs.lisp + 1] = "racket"
