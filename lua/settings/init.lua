0ff0abbd3b58f34595ea656a1692a36039857de1
Michael Espeña <lee.de.roy.124@gmail.com>
2021-12-05 14:25:25 +0800

    refactored settings; credits to mhartington

--- lua/settings/init.lua
+++ lua/settings/init.lua
@@ -1,33 +1,80 @@
--- Sane defaults
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = false
-vim.opt.updatetime = 100
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.wrap = false
-vim.opt.incsearch = true
-vim.opt.exrc = true
-vim.opt.scrolloff = 999
-vim.opt.sidescrolloff = 999
-vim.opt.hidden = true
-vim.opt.cursorline = true
-vim.opt.colorcolumn = "80"
-vim.opt.completeopt = "menuone,noselect"
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undofile = true
-vim.opt.termguicolors = true
-vim.opt.showmode = false
-vim.opt.signcolumn = "yes"
-vim.opt.timeoutlen = 500
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.list = true
-vim.opt.mouse = "a"
+-- settings
+local Type = { GLOBAL_OPTION = "o", WINDOW_OPTION = "wo", BUFFER_OPTION = "bo" }
+
+local add_options = function(option_type, options)
+	if type(options) ~= "table" then
+		error('options should be of type "table"')
+		return
+	end
+	local vim_option = vim[option_type]
+	for key, value in pairs(options) do
+		vim_option[key] = value
+	end
+end
+
+local Settings = {}
+
+Settings.g = function(options)
+	add_options(Type.GLOBAL_OPTION, options)
+end
+
+Settings.w = function(options)
+	add_options(Type.WINDOW_OPTION, options)
+end
+
+Settings.b = function(options)
+	add_options(Type.BUFFER_OPTION, options)
+end
+
+Settings.g({
+	tabstop = 4,
+	softtabstop = 4,
+	shiftwidth = 4,
+	expandtab = false,
+	updatetime = 100,
+	autoindent = true,
+	smartindent = true,
+	wrap = false,
+	incsearch = true,
+	exrc = true,
+	scrolloff = 999,
+	sidescrolloff = 999,
+	hidden = true,
+	cursorline = true,
+	colorcolumn = "80",
+	completeopt = "menu,menuone,noselect,noinsert",
+	swapfile = false,
+	backup = false,
+	undodir = "~/.local/share/undodir/nvim",
+	undofile = true,
+	termguicolors = true,
+	showmode = false,
+	timeoutlen = 500,
+	ignorecase = true,
+	smartcase = true,
+	list = true,
+	mouse = "a",
+	clipboard = "unnamedplus",
+	redrawtime = 500,
+	virtualedit = "onemore",
+	diffopt = "internal,filler,closeoff,algorithm:patience,iwhiteall",
+	splitbelow = true,
+	linebreak = true,
+	--[[ indentexpr = "nvim_treesitter#indent()", ]]
+})
+
+Settings.b({
+	swapfile = false,
+	shiftwidth = 4,
+})
+
+Settings.w({
+	number = true,
+	relativenumber = true,
+	signcolumn = "yes",
+	linebreak = true,
+	--[[ indentexpr = "nvim_treesitter#indent()", ]]
+})
 
 vim.g.wildignore = {
 	"*.git/*",
@@ -38,81 +85,4 @@ vim.g.wildignore = {
 	"*.ccls-cache",
 }
 
--- highlight yanked text
-vim.highlight.on_yank({ higroup = "IncSearch", timeout = 150 })
-
--- ignore files
-vim.opt.path:append({ I = "**/.git/*" })
-
--- remove the annoying tilde characters in empty lines (EOB)
-vim.cmd([[
-	let &fcs='eob: '
-]])
-
--- assembly programming settings (LOL. I don't know what I'm doing.)
-vim.cmd([[
-  autocmd BufNew,BufRead *.asm set ft=nasm
-]])
-
--- TAG JUMPING
--- With the command above we now can:
--- * Use ^] to jump to tag under cursor
--- * Use g^] for ambiguous tags
--- * Use ^t to jump back up the tag stack
---
--- Things to consider:
--- * This doesn't help if you want a visual list of tags
--- vim.cmd([[
---   command! MakeTags !ctags -R .
--- ]])
-
--- FINDING FILES
--- Search down into subfolders
--- Provides tab-completion for all file-related tasks
--- vim.opt.path:append{ I = "**"}
-
--- Display all matching files when we tab-complete
-vim.opt.wildmenu = true
-
--- The following configuration removes the pipes that
--- acts as separators on splits
--- NOTE: There is a SPACE typed in after the backslash
--- otherwise the pipes will just be replaced with
--- backlashes instead
--- vim.opt.fillchars:append { I = "vert:\ " }
-
--- With the last two configs set above we now can:
--- * hit TAB to :find by partial match
--- * use * to make it fuzzy
---
--- Things to consider:
--- * :b lets you autocomplete any open buffer
-
--- Tweaks for browsing using netrw
--- * :edit a folder to open a file browser
--- * <CR>/v/t to open in an h-split/v-split/tab
--- * check |netrw-browse-maps| for more mappings
--- vim.g.netrw_banner = 0				-- disable banner
--- vim.g.netrw_browse_split = 4		-- open in prior window
--- vim.g.netrw_altv = 1				-- open spilts to the right
--- vim.g.netrw_liststyle = 3			-- tree view
--- vim.g.netrw_list_hide = 'netrw_gitignore#Hide()'
--- vim.g.netrw_list_hide.=',\(^\|\s\s\)\zs\.\S\+'
-
--- source settings I still don't know how to translate to lua
--- TODO: Translate to Lua
-vim.cmd([[
-	set undodir=~/.local/share/undodir/nvim
-	
-	"let g:netrw_list_hide.=',\(^\|\s\s\)\zs\.\S\+'
-
-	" ---------- FIXME FIXME FIXME ---------- "
-	" The following configuration removes the pipes that
-	" acts as separators on splits
-	" WARNING: There is a SPACE typed in after the backslash
-	" otherwise the pipes will just be replaced with
-	" backlashes instead
-	" set fillchars+=vert:\ 
-
-	au BufNewFile,BufRead *.s,*.S set filetype=arm " arm = armv6/7
-]])
+return Settings
