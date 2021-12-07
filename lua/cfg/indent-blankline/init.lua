b216ce0f972c06f950d7719eef66bc857f680891
Michael Espeña <lee.de.roy.124@gmail.com>
2021-12-07 18:44:34 +0800

    added the right setting; yes I finally read the docs

--- lua/cfg/indent-blankline/init.lua
+++ lua/cfg/indent-blankline/init.lua
@@ -1,14 +1,9 @@
 vim.opt.list = true
--- NOTE: it doesn't show in the statusline what the buffer is called when
--- using builtin terminal so I just put an empty string which seems to do
--- the trick of disabling indent_blankline in terminal;
--- TODO: documentations show what the terminal buffer is called but I don't
--- know how to put the name properly here for the moment
 vim.g.indentLine_fileTypeExclude = {
 	"dashboard",
 	"packer",
 	"help",
 	"ToggleTerm",
-	"",
+	"terminal",
 }
 require("indent_blankline").setup({})
