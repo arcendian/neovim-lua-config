150bb04d3b08fbdf4a4dad5e7e67745114c16929
Michael Espeña <lee.de.roy.124@gmail.com>
2021-12-08 15:48:41 +0800

    add conf and yaml to hexokinase enabled filetypes

--- lua/cfg/hex-colors/init.lua
+++ lua/cfg/hex-colors/init.lua
@@ -1,3 +1,3 @@
 -- =========================== HEXOKINASE ================================== --
 vim.g.Hexokinase_highlighters = { "backgroundfull" }
-vim.g.Hexokinase_ftEnabled = { "css", "html", "javascript" }
+vim.g.Hexokinase_ftEnabled = { "css", "html", "javascript", "conf", "yaml" }
