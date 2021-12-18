e655bc0c7c0f793a0eb43102c0cebd3b89a8c03e
Michael Espeña <lee.de.roy.124@gmail.com>
2021-12-18 10:19:11 +0800

    changed exclusion settings

--- lua/cfg/indent-blankline/init.lua
+++ lua/cfg/indent-blankline/init.lua
@@ -1,11 +1,16 @@
-vim.opt.list = true
+vim.g.indent_blankline_max_indent_increase = 1
 
 vim.g.indentLine_fileTypeExclude = {
 	"dashboard",
 	"packer",
 	"help",
-	"ToggleTerm",
+}
+
+vim.g.indentLine_bufTypeExclude = {
 	"terminal",
+}
+vim.g.indent_blankline_bufname_exclude = {
+	"%.md",
 }
 
 require("indent_blankline").setup({})
