dd8d5a09ebfd83a4ebc6236392f072dcf4175706
Michael Espeña <lee.de.roy.124@gmail.com>
2021-12-22 18:13:55 +0800

    override colors in colors setting

--- lua/cfg/tree-sitter/init.lua
+++ lua/cfg/tree-sitter/init.lua
@@ -42,16 +42,14 @@ treesitter_cfg.setup({
 		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
 		max_file_lines = nil, -- Do not enable for files with more than n lines, int
 		-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-		-- colors = {}, -- table of hex strings
+		colors = {
+			"#e06c75",
+			"#d19a66",
+			"#ebcb8b",
+			"#b988d0",
+			"#a3b38c",
+			"#88c0d0",
+		},
 		-- termcolors = {} -- table of colour name strings
 	},
 })
-
-vim.cmd([[
-	hi rainbowcol1 guifg=#e06c75
-	hi rainbowcol2 guifg=#d19a66
-	hi rainbowcol3 guifg=#ebcb8b
-	hi rainbowcol4 guifg=#b988d0
-	hi rainbowcol5 guifg=#a3b38c
-	hi rainbowcol6 guifg=#88c0d0
-]])
