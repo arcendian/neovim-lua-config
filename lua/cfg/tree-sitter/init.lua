61f05a6c8e23decd97af833b17e91d3ab5c5f63b
Michael Espeña <lee.de.roy.124@gmail.com>
2021-12-22 18:01:32 +0800

    trying out ts-rainbow

--- lua/cfg/tree-sitter/init.lua
+++ lua/cfg/tree-sitter/init.lua
@@ -1,4 +1,9 @@
-require("nvim-treesitter.configs").setup({
+local status_ok, treesitter_cfg = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+	return
+end
+
+treesitter_cfg.setup({
 	ensure_installed = {
 		"bash",
 		"c",
@@ -32,4 +37,21 @@ require("nvim-treesitter.configs").setup({
 		"zig",
 	},
 	highlight = { enable = true, additional_vim_regex_highlighting = false },
+	rainbow = {
+		enable = true,
+		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+		max_file_lines = nil, -- Do not enable for files with more than n lines, int
+		-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+		-- colors = {}, -- table of hex strings
+		-- termcolors = {} -- table of colour name strings
+	},
 })
+
+vim.cmd([[
+	hi rainbowcol1 guifg=#e06c75
+	hi rainbowcol2 guifg=#d19a66
+	hi rainbowcol3 guifg=#ebcb8b
+	hi rainbowcol4 guifg=#b988d0
+	hi rainbowcol5 guifg=#a3b38c
+	hi rainbowcol6 guifg=#88c0d0
+]])
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -82,6 +82,7 @@ return packer.startup(function()
 	-- ========================== UI ====================================== --
 	-- better syntax highlighting with tree-sitter and more
 	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+	use({ "p00f/nvim-ts-rainbow" })
 
 	-- colorscheme
 	use({ "rmehri01/onenord.nvim" })
