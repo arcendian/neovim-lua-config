a40aadb5f03bba102abdffea1a0faac28285e4ea
Michael Espeña <michael.esp42@protonmail.com>
2021-12-27 09:06:49 +0800

    disable taplo for now

--- lua/cfg/lspconfig/init.lua
+++ lua/cfg/lspconfig/init.lua
@@ -56,7 +56,7 @@ local servers = {
 	"hls",
 	"arduino_language_server",
 	"sumneko_lua",
-	"taplo",
+	-- "taplo",
 	"zls",
 }
 local cmp_capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
