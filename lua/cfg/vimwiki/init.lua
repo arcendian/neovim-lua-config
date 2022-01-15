cfbf39a8a44856b66cb066dd656fe9a70e1ddfb1
Michael Espeña <michael.esp42@protonmail.com>
2022-01-16 06:36:12 +0800

    rm TODO comment

--- lua/cfg/vimwiki/init.lua
+++ lua/cfg/vimwiki/init.lua
@@ -15,16 +15,3 @@ vim.cmd([[
 ]])
 
 vim.g.vimwiki_global_ext = 0
-
--- TODO: How to translate the above to Lua. Too lazy to search for now.
--- vim.g.vimwiki_list = {
---     path = '~/Documents/VimWiki/Default/',
---     path_tasks = '~/Documents/VimWiki/Tasks/',
---     path_calendar = '~/Documents/VimWiki/Calendar/',
---     path_others = '~/Documents/VimWiki/Others',
---     path_learning = '~/Documents/VimWiki/Learning/',
---     path_podcasts = '~/Documents/VimWiki/Podcasts/',
---     path_random = '~/Documents/VimWiki/Random/',
---     path_review = '~/Documents/VimWiki/Review/',
---     path_worldbuilding = '~/Documents/VimWiki/Worldbuilding/',
--- }
