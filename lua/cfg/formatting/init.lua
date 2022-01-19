0911bfc72412cf1364304a052fc90820e8b8d4e4
Michael Espeña <michael.esp42@protonmail.com>
2022-01-19 19:35:38 +0800

    use chromium style for clang-format for now

--- lua/cfg/formatting/init.lua
+++ lua/cfg/formatting/init.lua
@@ -27,7 +27,7 @@ local formatterConfig = {
 			return {
 				exe = "clang-format",
 				args = {
-					"-style='{BasedOnStyle: gnu, IndentWidth: 4, AccessModifierOffset: -4, BreakBeforeBraces: Linux, AlwaysBreakAfterDefinitionReturnType: None, AlwaysBreakAfterReturnType: None, AllowShortFunctionsOnASingleLine: None}' --assume-filename",
+					"-style='{BasedOnStyle: chromium, IndentWidth: 4, AccessModifierOffset: -4}' --assume-filename",
 					vim.api.nvim_buf_get_name(0),
 				},
 				stdin = true,
@@ -41,7 +41,7 @@ local formatterConfig = {
 			return {
 				exe = "clang-format",
 				args = {
-					"-style='{BasedOnStyle: gnu, IndentWidth: 4, AccessModifierOffset: -4, BreakBeforeBraces: Linux, AlwaysBreakAfterDefinitionReturnType: None, AlwaysBreakAfterReturnType: None, AllowShortFunctionsOnASingleLine: None}' --assume-filename",
+					"-style='{BasedOnStyle: chromium, IndentWidth: 4, AccessModifierOffset: -4}' --assume-filename",
 					vim.api.nvim_buf_get_name(0),
 				},
 				stdin = true,
@@ -55,7 +55,7 @@ local formatterConfig = {
 			return {
 				exe = "clang-format",
 				args = {
-					"-style='{BasedOnStyle: google, IndentWidth: 4, AccessModifierOffset: -4}' --assume-filename",
+					"-style='{BasedOnStyle: chromium, IndentWidth: 4, AccessModifierOffset: -4}' --assume-filename",
 					vim.api.nvim_buf_get_name(0),
 				},
 				stdin = true,
@@ -113,7 +113,7 @@ local formatterConfig = {
 			return {
 				exe = "clang-format",
 				args = {
-					"-style='{BasedOnStyle: google, IndentWidth: 4, AccessModifierOffset: -4}' --assume-filename",
+					"-style='{BasedOnStyle: chromium, IndentWidth: 4, AccessModifierOffset: -4}' --assume-filename",
 					vim.api.nvim_buf_get_name(0),
 				},
 				stdin = true,
