a64627bb644ff55a0f601ac2eae474f63d7576ec
Michael Espeña <michael.esp42@protonmail.com>
2022-01-20 17:13:15 +0800

    added vim.diagnostics config
    - disable virtual text
    - added cosmetics configuration

--- lua/cfg/lspconfig/init.lua
+++ lua/cfg/lspconfig/init.lua
@@ -134,11 +134,42 @@ for _, lsp in ipairs(servers) do
 end
 
 -- =================== LSP diagnostics customization ======================= --
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-	virtual_text = {
-		prefix = "●", -- Could be '●', '▎', 'x'
+
+local config = {
+	-- disable virtual text
+	virtual_text = false,
+	-- show signs
+	signs = {
+		active = signs,
 	},
+	update_in_insert = true,
+	underline = true,
+	severity_sort = true,
+	float = {
+		focusable = false,
+		style = "minimal",
+		border = "rounded",
+		source = "always",
+		header = "",
+		prefix = "",
+	},
+}
+
+vim.diagnostic.config(config)
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+	border = "rounded",
 })
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+	border = "rounded",
+})
+
+-- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+-- 	virtual_text = {
+-- 		prefix = "●", -- Could be '●', '▎', 'x'
+-- 	},
+-- })
 
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 
