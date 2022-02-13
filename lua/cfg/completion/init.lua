3603dc1c68c742949c20374196772483d53fffc1
Michael Espeña <michael.esp42@protonmail.com>
2022-02-13 10:38:36 +0800

    replaced vsnip with luasnips
    - installed luasnips
    - added initial settings for luasnips

--- lua/cfg/completion/init.lua
+++ lua/cfg/completion/init.lua
@@ -3,6 +3,16 @@ if not status_ok then
 	return
 end
 
+local luasnip_loaded, luasnip = pcall(require, "luasnip")
+if not luasnip_loaded then
+	return
+end
+
+local has_words_before = function()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 local kind_icons = {
 	Text = "",
 	Method = "",
@@ -34,7 +44,7 @@ local kind_icons = {
 cmp.setup({
 	snippet = {
 		expand = function(args)
-			vim.fn["vsnip#anonymous"](args.body)
+			require("luasnip").lsp_expand(args.body)
 		end,
 	},
 	mapping = {
@@ -43,11 +53,33 @@ cmp.setup({
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.close(),
 		["<C-k>"] = cmp.mapping.confirm({ select = true }),
+
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
 	},
 	sources = {
 		{ name = "nvim_lsp" },
 		{ name = "tree-sitter" },
-		{ name = "vsnip" },
+		{ name = "luasnip" },
 		{ name = "buffer" },
 		{ name = "path" },
 		{ name = "cmdline" },
