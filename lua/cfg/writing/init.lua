44d3cd805c00a452da0d770b9b2020c6a60e3c82
Michael Espeña <michael.esp42@protonmail.com>
2022-02-13 10:37:59 +0800

    added minor settings tweaks when toggline zenmode

--- lua/cfg/writing/init.lua
+++ lua/cfg/writing/init.lua
@@ -40,15 +40,25 @@ zen_mode.setup({
 			list = false,
 		},
 	},
-	plugins = { options = { enabled = true, ruler = false, showcmd = false } },
+	plugins = {
+		options = {
+			enabled = true,
+			ruler = false,
+			showcmd = false,
+		},
+	},
 	on_open = function(win)
 		vim.opt_local.colorcolumn = "0"
+		vim.opt_local.number = false
+		vim.opt_local.relativenumber = false
 		vim.opt_local.wrap = true
 		vim.opt_local.spell = true
 		vim.opt_local.spelllang = "en"
 	end,
 	on_close = function()
 		vim.opt_local.colorcolumn = "80"
+		vim.opt_local.number = true
+		vim.opt_local.relativenumber = true
 		vim.opt_local.wrap = false
 		vim.opt_local.spell = false
 	end,
