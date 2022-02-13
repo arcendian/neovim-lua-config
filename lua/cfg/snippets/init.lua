ef97353b909eaaaabe80bfedce10f773c75f6788
Michael Espeña <michael.esp42@protonmail.com>
2022-02-13 11:25:40 +0800

    added initial luasnip settings

---
+++ lua/cfg/snippets/init.lua
@@ -0,0 +1,32 @@
+local status_ok, ls = pcall(require, "luasnip")
+if not status_ok then
+	return
+end
+
+local types = require("luasnip.util.types")
+
+require("luasnip.loaders.from_vscode").load()
+
+ls.config.set_config({
+	-- Remember to keep around the last snippet so you can jump back into it
+	-- even if you move outside of the selection
+	history = true,
+
+	-- update dynamic snippets as you type
+	updateevents = "TextChanged, TextChangedI",
+
+	-- autosnippets
+	enable_autosnippets = true,
+})
+
+ls.snippets = {
+	all = {
+		ls.parser.parse_snippet("myname", "Michael Espeña"),
+		ls.parser.parse_snippet("myemail", "michaelespena.public@gmail.com"),
+	},
+
+	lua = {
+		ls.parser.parse_snippet("lf", "local $1 = function($2)\n  $0\nend"),
+		ls.parser.parse_snippet("mf", "$1.$2 = function($3)\n  $0\nend"),
+	},
+}
