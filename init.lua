62198f825e532d6f62522a97fc29b57e33e26a21
Michael Espeña <michael.esp42@protonmail.com>
2022-07-18 20:58:15 +0800

    removed the for loop; require by line

--- init.lua
+++ init.lua
@@ -11,33 +11,23 @@ require("plugins")
 require("settings")
 require("remaps")
 
-local cgf_dir = "cfg"
-local configs = {
-	"theme",
-	"statusline",
-	"terminal",
-	"autopairs",
-	"completion",
-	"writing",
-	"dashboard",
-	"filetree",
-	"devicons",
-	"comment",
-	"tree-sitter",
-	"lspconfig",
-	"telescope",
-	"gitsigns",
-	"formatting",
-	"vimwiki",
-	"hex-colors",
-	"autocmds",
-	"which-key",
-	"neogit",
-	"diffview",
-	"notify",
-	"snippets",
-}
-
-for _, config in ipairs(configs) do
-	require(cgf_dir .. "." .. config)
-end
+require("cfg.theme")
+require("cfg.statusline")
+require("cfg.terminal")
+require("cfg.autopairs")
+require("cfg.completion")
+require("cfg.filetree")
+require("cfg.devicons")
+require("cfg.comment")
+require("cfg.tree-sitter")
+require("cfg.lspconfig")
+require("cfg.telescope")
+require("cfg.gitsigns")
+require("cfg.formatting")
+require("cfg.undotree")
+require("cfg.hex-colors")
+require("cfg.autocmds")
+require("cfg.which-key")
+require("cfg.neogit")
+require("cfg.notify")
+require("cfg.snippets")
